a34924a10b6cabfa72eef3eb6a50c479cb507d6d
expertglacier <118358272+expertglacier@users.noreply.github.com>
2022-12-12 13:11:39 +0700

    Add files via upload

---
+++ exercise_1929/bin/exercise_1929.dart
@@ -0,0 +1,7 @@
+// 1929. Concatenation of Array
+
+class Solution {
+  List<int> getConcatenation(List<int> nums) {
+    return nums + nums;
+  }
+}
---
+++ exercise_1929/test/exercise_1929_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercise_1929/exercise_1929.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
